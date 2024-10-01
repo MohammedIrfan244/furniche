6571488c58f2c9becda5d508f89b4d6455472232
user-irfan <mohammedirfannedumba@gmail.com>
2024-10-01 17:05:04 +0530

    cart

--- app/src/Pages/Cart.jsx
+++ app/src/Pages/Cart.jsx
@@ -4,9 +4,13 @@ import { ShopContext } from "../Contexts/ShopContext"
 
 function Cart() {
   const{removeCart,currency,cartItems}=useContext(ShopContext)
-  console.log("hello");
+  console.log("hello world");
   
   console.log(cartItems);
+  console.log(removeCart);
+  console.log(currency);
+  
+  
   
   return (
     <div>
