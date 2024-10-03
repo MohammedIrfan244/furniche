1f20b9ca5ea63f58027fd1891821daaa88196142
user-irfan <mohammedirfannedumba@gmail.com>
2024-10-03 12:33:14 +0530

    userFirst

--- app/src/Pages/LoginSignUp.jsx
+++ app/src/Pages/LoginSignUp.jsx
@@ -1,15 +1,20 @@
 import { useContext } from "react"
 import { UserContext } from "../Contexts/UserContext"
+import { Link } from "react-router-dom"
 
 
 function LoginSignUp() {
   const{isLoggedIn,setIsLoggedIn}=useContext(UserContext)
   return (
     <div>
-      <form action="">
-      <input type="text" placeholder="username" className="" />
-      <input type="email" placeholder="emal"/>
-      <input type="password" placeholder="password"/>
+      <form >
+      <input type="text" required placeholder="username" className={`${isLoggedIn==false?'hidden':null}`} />
+      <input type="email" required placeholder="emal"/>
+      <input type="password" required placeholder="password"/>
+      <Link onClick={()=>setIsLoggedIn(!isLoggedIn)} className={`${isLoggedIn?'hidden':null}`}>dont have an account ?</Link>
+      <Link onClick={()=>setIsLoggedIn(!isLoggedIn)} className={`${isLoggedIn==false?'hidden':null}`}>already have an account ?</Link>
+      <button type="submit" className={`${isLoggedIn?'hidden':null}`}>Sign in</button>
+      <button type="submit" className={`${isLoggedIn==false?'hidden':null}`}>Sign up</button>
       </form>
     </div>
   )
