b6cb7cca1bd706ace3f1408ddac15fc5e0176f7f
user-irfan <mohammedirfannedumba@gmail.com>
2024-10-05 03:02:25 +0530

    final

--- app/src/Pages/NotFound.jsx
+++ app/src/Pages/NotFound.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+
 
 function NotFound() {
   return (
-    <div className='pt-[30%] sm:pt-[10%]'>NotFound</div>
+    <div className='pt-[30%] sm:pt-[10%]'>
+      <h1 className="text-2xl text-center text-gray-700 mt-5">OOPS...We couldn't find the page you are looking for</h1>
+    </div>
   )
 }
 
